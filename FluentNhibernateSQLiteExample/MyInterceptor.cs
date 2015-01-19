049a6f400e7dc4ef884f68e741f4492bfca461e7
OrennSchwartz <OrenSchwartz@gmail.com>
2015-01-20 01:09:04 +0200

    my nhibernate test

---
+++ FluentNhibernateSQLiteExample/MyInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentNhibernateSQLiteExample
+{
+    public class MyInterceptor : EmptyInterceptor
+    {
+        public override bool OnSave(object entity, object id, object[] state, string[] propertyNames, NHibernate.Type.IType[] types)
+        {
+            return base.OnSave(entity, id, state, propertyNames, types);
+        }
+        public override bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, NHibernate.Type.IType[] types)
+        {
+            return base.OnFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+        }
+
+        public override NHibernate.SqlCommand.SqlString OnPrepareStatement(NHibernate.SqlCommand.SqlString sql)
+        {
+            return base.OnPrepareStatement(sql);
+        }
+    }
+}
